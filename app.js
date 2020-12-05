61dafcec5fa5f8075537f974eaa85c103453304f
Aamer Desai <56724460+minm333@users.noreply.github.com>
2020-12-05 18:06:19 -0500

    Create variables for required elements

---
+++ app.js
@@ -0,0 +1,9 @@
+// Elements
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+// Ticket Price
+const ticketPrice = +movieSelect.value;
