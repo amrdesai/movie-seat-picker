16f16f5f6fdd85d77b50e38bad337ded67fdd47e
Aamer Desai <56724460+minm333@users.noreply.github.com>
2020-12-05 18:29:06 -0500

    Add selected functionality and update count, total on UI

--- app.js
+++ app.js
@@ -6,4 +6,32 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 // Ticket Price
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
+
+// Function: Update total & count
+const updateSelectedCount = () => {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
+    // Update count in UI
+    count.innerText = selectedSeatsCount;
+    // Update total in UI
+    total.innerText = `$${(selectedSeatsCount * ticketPrice).toFixed(2)}`;
+};
+
+// Event Listener: Movie elect event
+movieSelect.addEventListener('change', (e) => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+});
+
+// Event Listener: On seat selection
+container.addEventListener('click', (e) => {
+    if (
+        e.target.classList.contains('seat') &&
+        !e.target.classList.contains('occupied')
+    ) {
+        e.target.classList.toggle('selected');
+
+        updateSelectedCount();
+    }
+});
