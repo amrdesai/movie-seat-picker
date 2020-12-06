67b4c4511d070297e291bf405a58cb2fb015637c
Aamer Desai <56724460+minm333@users.noreply.github.com>
2020-12-06 18:45:13 -0500

    Add functionality to save selected seats to localStorge and load selected seats on page load

--- app.js
+++ app.js
@@ -5,23 +5,59 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+// Populate UI
+populateUI();
+
 // Ticket Price
 let ticketPrice = +movieSelect.value;
 
 // Function: Update total & count
-const updateSelectedCount = () => {
+const updateSelectedCountAndTotal = () => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     const selectedSeatsCount = selectedSeats.length;
+
+    // Get indexes
+    const seatsIndex = [...selectedSeats].map((seat) =>
+        [...seats].indexOf(seat)
+    );
+    // Save to localStorage
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     // Update count in UI
     count.innerText = selectedSeatsCount;
     // Update total in UI
     total.innerText = `$${(selectedSeatsCount * ticketPrice).toFixed(2)}`;
 };
 
-// Event Listener: Movie elect event
+// Function: Save movie & price to localStorage
+const setMovieDataToLocalStorage = (movieIndex, moviePrice) => {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
+// Function: Get data from LocalStorage & populate UI on page load
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
+// Event Listener: Movie select event
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = +e.target.value;
-    updateSelectedCount();
+    setMovieDataToLocalStorage(e.target.selectedIndex, e.target.value);
+    updateSelectedCountAndTotal();
 });
 
 // Event Listener: On seat selection
@@ -32,6 +68,9 @@ container.addEventListener('click', (e) => {
     ) {
         e.target.classList.toggle('selected');
 
-        updateSelectedCount();
+        updateSelectedCountAndTotal();
     }
 });
+
+// Initial count and total set
+updateSelectedCountAndTotal();
